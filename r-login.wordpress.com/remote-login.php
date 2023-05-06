f104eaf48087e4722aec1713c7dcac588db35a17
Deepraj <83288891+deepraj21@users.noreply.github.com>
2023-05-07 04:50:42 +0530

    Add files via upload

---
+++ r-login.wordpress.com/remote-login.php
@@ -0,0 +1,4 @@
+
+<script class="wpcom_remote_login_msg">
+window.parent.postMessage( '{"msg":"REQUEST_ACCESS","origin":"aHR0cHM6Ly9kdWNrZGVzay5jb2RlLmJsb2c=","wpcomid":"197094453"}', 'https://duckdesk.code.blog' );
+</script>
